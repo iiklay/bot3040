00e2ecb6acaa10b0b3c0c224cb59b28cb972e346
iiklay <40916077+iiklay@users.noreply.github.com>
2019-03-24 09:18:07 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -26,4 +26,35 @@
             return;
         }
     });
+const Discord = require('discord.js');
+
+const Util = require('discord.js');
+    client = new Discord.Client({sisableEveryone: true})
+ const devs = ['525375324863594496'];
+const adminprefix = "-";
+client.on('message', message => {
+    var argresult = message.content.split(` `).slice(1).join(' ');
+      if (!devs.includes(message.author.id)) return;
+      
+  if (message.content.startsWith(adminprefix + 'playing')) {
+    client.user.setGame(argresult);
+      message.channel.sendMessage(`**  ${argresult} اتغيرت حالتك الى :white_check_mark:**`)
+  } else 
+  if (message.content.startsWith(adminprefix + 'watching')) {
+  client.user.setActivity(argresult, {type:'WATCHING'});
+      message.channel.sendMessage(`**  ${argresult} اتغيرت حالتك الى :white_check_mark:**`)
+  } else 
+  if (message.content.startsWith(adminprefix + 'listen')) {
+  client.user.setActivity(argresult , {type:'LISTENING'});
+      message.channel.sendMessage(`**  ${argresult} اتغيرت حالتك الى :white_check_mark:**`)
+  } else 
+  if (message.content.startsWith(adminprefix + 'streaming')) {
+    client.user.setGame(argresult, "https://www.twitch.tv/raid__1");
+      message.channel.sendMessage(`**  ${argresult} اتغيرت حالتك الى :white_check_mark:**`)
+  }
+  });
+
+
+
+
  client.login(process.env.BOT_TOKEN);
