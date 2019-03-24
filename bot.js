9c4c847dfe48c01ada480a2f012ec4727ce7c24c
iiklay <40916077+iiklay@users.noreply.github.com>
2019-03-24 09:20:07 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -26,6 +26,8 @@
             return;
         }
     });
+
+
 const Discord = require('discord.js');
 
 const Util = require('discord.js');
@@ -36,19 +38,19 @@ client.on('message', message => {
     var argresult = message.content.split(` `).slice(1).join(' ');
       if (!devs.includes(message.author.id)) return;
       
-  if (message.content.startsWith(adminprefix + 'playing')) {
+  if (message.content.startsWith(adminprefix + 'play')) {
     client.user.setGame(argresult);
       message.channel.sendMessage(`**  ${argresult} اتغيرت حالتك الى :white_check_mark:**`)
   } else 
-  if (message.content.startsWith(adminprefix + 'watching')) {
+  if (message.content.startsWith(adminprefix + 'wat')) {
   client.user.setActivity(argresult, {type:'WATCHING'});
       message.channel.sendMessage(`**  ${argresult} اتغيرت حالتك الى :white_check_mark:**`)
   } else 
-  if (message.content.startsWith(adminprefix + 'listen')) {
+  if (message.content.startsWith(adminprefix + 'lis')) {
   client.user.setActivity(argresult , {type:'LISTENING'});
       message.channel.sendMessage(`**  ${argresult} اتغيرت حالتك الى :white_check_mark:**`)
   } else 
-  if (message.content.startsWith(adminprefix + 'streaming')) {
+  if (message.content.startsWith(adminprefix + 'set')) {
     client.user.setGame(argresult, "https://www.twitch.tv/raid__1");
       message.channel.sendMessage(`**  ${argresult} اتغيرت حالتك الى :white_check_mark:**`)
   }
